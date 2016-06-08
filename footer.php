710a260ff560dbd1b46d414f4b0700ff16b9016e
Antoine Floris <antoine.floris@gmail.com>
2016-06-09 00:53:00 +0200

    include inserer dans mon index.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="row">
+        <div class="col-xs-12">
+            <a href="contact.html">Contact</a> - <a href="mentions.html">Mentions légales</a> - <a href="https://www.facebook.com/infoprog.tuto">Facebook</a>
+        </div>
+    </div>
+</footer>
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AF-entrainement-php</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,300,700">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-2">
+                    <a href="index.php">AF tonio</a>
+                </div>
+                <div class="col-sm-10">
+                    <nav>
+                        <ul>
+                            <li><a href="index.php">Accueil</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Connexion</a></li>
+                            <li><a href="#">Inscription</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
