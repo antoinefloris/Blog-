2fb75f8b4a05593c080f816a3c593ce2b123c112
Antoine Floris <antoine.floris@gmail.com>
2016-06-09 10:45:43 +0200

    ajout de trois articles

--- fonctions/bdd.php
+++ fonctions/bdd.php
@@ -1,7 +1,7 @@
 <?php
 function bdd(){
     try {
-        $bdd = new PDO("mysql:dbname=blog;host=localhost", "root", "root");
+        $bdd = new PDO("mysql:dbname=blog;host=localhost", "root", "root", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
     } catch (PDOException $e) {
         echo 'Connection echouée : ' . $e->getMessage();
     }
