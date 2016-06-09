a66c5581c357fba14d4fd0b16dcc4c76715bd910
Antoine Floris <antoine.floris@gmail.com>
2016-06-09 10:44:28 +0200

    ajout du titre

--- header.php
+++ header.php
@@ -1,10 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AF-entrainement-php</title>
+    <meta charset="UTF-8">
+    <title>entraînement-php</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,300,700">
     <link rel="stylesheet" href="main.css">
